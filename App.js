905f3c6b8367a271aa1e592c3622d87d18274291
dengshaomin86 <1101869752@qq.com>
2022-04-19 23:27:38 +0800

    init

--- App.js
+++ App.js
@@ -1,18 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Provider } from '@ant-design/react-native';
-import Nav from './src/views/Nav';
+import { WebView } from 'react-native-webview';
+import { StatusBar, StyleSheet, View } from 'react-native';
 
 class App extends Component {
+  state = {
+    routerName: '',
+    backgroundColor: '#2979ff',
+    barStyle: 'light-content',
+  };
+
+  onMessage(event) {
+    let { data } = event.nativeEvent;
+    data = JSON.parse(data);
+    const { routerName } = data;
+    const isDark = ['SignIn', 'SignUp'].includes(routerName);
+    const backgroundColor = isDark ? '#ffffff' : '#2979ff';
+    const barStyle = isDark ? 'dark-content' : 'light-content';
+    this.setState({ routerName, backgroundColor, barStyle });
+  }
+
   render() {
+    const { backgroundColor, barStyle } = this.state;
+    // const uri = 'file:///android_asset/static.bundle/index.html';
+    // const uri = 'file:///android_asset/dist/index.html';
+    const uri = 'http://42.194.207.119/';
     return (
-      <Provider>
-        <View style={{ flex: 1, backgroundColor: '#2B2C31' }}>
-          <Nav></Nav>
-        </View>
-      </Provider>
+      <View style={styles.wrapper}>
+        <StatusBar backgroundColor={backgroundColor} barStyle={barStyle} translucent={false} />
+        <WebView source={{ uri }} originWhitelist={['*']} onMessage={this.onMessage.bind(this)} />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+});
 
 export default App;
--- index.js
+++ index.js
@@ -2,11 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 // 使调试能捕获网络请求
-global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
+// global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
 
 AppRegistry.registerComponent(appName, () => App);
